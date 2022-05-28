9b4b8ddba714928b39f96b4a055421e38b7d29b6
sns-ikeda <ikeda.shunnosuke@gmail.com>
2022-05-28 17:47:10 +0900

    plotを修正

--- src/utils/plot.py
+++ src/utils/plot.py
@@ -8,23 +8,30 @@ import seaborn as sns
 
 
 def save_image(
-    calculation_time_dict: dict[int, float],
+    avg_results_dict: dict[str, dict[int, float]],
     dir_path: Union[str, Path],
     image_name: str,
-    time_limit: int,
+    y_label: str,
+    x_label: str = "num of items",
 ) -> None:
-    x = list(calculation_time_dict.keys())
-    y = list(calculation_time_dict.values())
+
     plt.style.use("default")
     sns.set()
     sns.set_style("whitegrid")
     sns.set_palette("gray")
+    sns.set_palette("Set1")
     fig = plt.figure()
     ax = fig.add_subplot(1, 1, 1)
+    y_max = 0
+    for method, avg_results_dict_method in avg_results_dict.items():
+        x = list(avg_results_dict_method.keys())
+        y = list(avg_results_dict_method.values())
+        ax.plot(x, y, marker="o", label=method)
+        if max(y) > y_max:
+            y_max = max(y)
 
-    ax.plot(x, y, marker="o")
-
-    ax.set_xlabel("num of items")
-    ax.set_ylabel("calculation time [sec]")
-    ax.set_ylim(0, time_limit * 1.1)
+    ax.legend()
+    ax.set_xlabel(x_label)
+    ax.set_ylabel(y_label)
+    ax.set_ylim(0, y_max * 1.1)
     fig.savefig(Path(dir_path) / f"{image_name}.png", format="png", dpi=300)
