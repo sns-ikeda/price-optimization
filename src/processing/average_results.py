ebf88d4dde08f1bb3e4d790c98ddd986f7648dd5
sns-ikeda <ikeda.shunnosuke@gmail.com>
2022-05-28 16:56:54 +0900

    結果の処理を汎用化

---
+++ src/processing/average_results.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+
+import numpy as np
+
+from algorithm.result import Result
+
+
+def average_results(
+    results_dict: dict[str, dict[int, list[Result]]], attribute: str
+) -> dict[str, dict[int, float]]:
+    """各手法ごとの計算結果を平均する"""
+    avg_results_dict = dict()
+    for method, results_dict_method in results_dict.items():
+        avg_results_dict[method] = average_results_method(
+            results_dict_method=results_dict_method, attribute=attribute
+        )
+    return avg_results_dict
+
+
+def average_results_method(
+    results_dict_method: dict[int, list[Result]], attribute: str
+) -> dict[int, float]:
+    """手法単位の計算結果を平均する"""
+    avg_values_dict = dict()
+    for num_of_items, results in results_dict_method.items():
+        values = []
+        for result in results:
+            values.append(getattr(result, attribute))
+        avg_values_dict[num_of_items] = np.mean(values).round(3)
+    return avg_values_dict
--- src/processing/dict2df.py
+++ src/processing/dict2df.py
@@ -1,12 +1,16 @@
 from __future__ import annotations
 
+from typing import Any
+
 import pandas as pd
 
 
-def dict2df(dict_: dict[int, float], value_name: str) -> pd.DataFrame:
+def dict2df(
+    dict_: dict[Any, Any], value_name: str, index_name: str = "num_of_items"
+) -> pd.DataFrame:
     result_df = (
         pd.DataFrame.from_dict(dict_, orient="index")
         .reset_index()
-        .rename(columns={"index": "num_of_items", 0: value_name})
+        .rename(columns={"index": index_name, 0: value_name})
     )
     return result_df.round(3)
