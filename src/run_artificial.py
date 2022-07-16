c7395252f1387340f8d10370296b196034b32852
sns-ikeda <ikeda.shunnosuke@gmail.com>
2022-07-16 13:04:46 +0900

    結果の保存方法を変更

--- src/run_artificial.py
+++ src/run_artificial.py
@@ -10,6 +10,7 @@ from src.optimization.processing.result_processor import (
     transform_artificial_results,
 )
 from src.utils.dict_converter import dict2json
+from src.utils.flatten_dict import flatten_dict
 from src.utils.paths import RESULT_DIR
 from src.utils.plot import save_image
 
@@ -39,20 +40,34 @@ def main():
             results=results, attribute=result_prefix
         )
         avg_results_dict = average_results(results_dict=transformed_results_dict)
+        flattened_results_dict = flatten_dict(avg_results_dict)
+        # 結果を保存
+        save_name = (
+            f"{result_prefix}_K{params['num_of_prices']}_"
+            + f"D{params['num_of_other_features']}_DoT{params['depth_of_trees']}"
+        )
+        # jsonで保存
+        dict2json(
+            target_dict=avg_results_dict, save_path=RESULT_DIR / "json" / (save_name + ".json")
+        )
+        # png形式で保存
+        if result_prefix == "calculation_time":
+            y_label = "calculation time [sec]"
+        else:
+            y_label = result_prefix
+        save_image(
+            avg_results_dict=flattened_results_dict,
+            dir_path=RESULT_DIR / "png",
+            image_name=save_name,
+            y_label=y_label,
+        )
+        # モデルごとの結果を保存
         for model, avg_results_dict_ in avg_results_dict.items():
             save_name = (
                 f"{result_prefix}_{model}_K{params['num_of_prices']}_"
                 + f"D{params['num_of_other_features']}_DoT{params['depth_of_trees']}"
             )
-            # jsonで保存
-            dict2json(
-                target_dict=avg_results_dict_, save_path=RESULT_DIR / "json" / (save_name + ".json")
-            )
             # png形式で保存
-            if result_prefix == "calculation_time":
-                y_label = "calculation time [sec]"
-            else:
-                y_label = result_prefix
             save_image(
                 avg_results_dict=avg_results_dict_,
                 dir_path=RESULT_DIR / "png",
