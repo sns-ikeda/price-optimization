035c1377d275dce0f1ac1f776766ed4e88fdac47
sns-ikeda <ikeda.shunnosuke@gmail.com>
2022-08-09 22:23:20 +0900

    POLRの実装

---
+++ src/optimize/models/POLR/make_input.py
@@ -0,0 +1,36 @@
+from __future__ import annotations
+
+import itertools
+
+from src.optimize.models.POLR.model import Constant, IndexSet
+from src.optimize.params import ArtificialDataParameter, RealDataParameter
+
+
+def make_artificial_input(params: ArtificialDataParameter) -> tuple[IndexSet, Constant]:
+    """人工的にモデルのパラメータを生成"""
+    index_set, constant = None, None
+    return index_set, constant
+
+
+def make_real_input(params: RealDataParameter) -> tuple[IndexSet, Constant]:
+    """実際のデータからモデルのパラメータを生成"""
+    M = list(params.item2prices.keys())
+    K = list(range(params.num_of_prices))
+    P = dict()
+    for m, k in itertools.product(M, K):
+        P[m, k] = params.item2prices[m][k]
+    phi = {(m, mp, k): 1 for m, mp, k in itertools.product(M, M, K)}
+
+    price_cols = ["PRICE" + "_" + m for m in M]
+    beta, g, D, D_ = dict(), dict(), dict(), dict()
+    for m in M:
+        predictor = params.item2predictor[m]
+        for col, coef in predictor.coef_dict.items():
+            beta[m, col] = coef
+            g[col] = params.g[col]
+        g["intercept"] = 1
+        D[m] = [col for col in predictor.coef_dict.keys() if col not in price_cols]
+        D_[m] = ["intercept"]
+    index_set = IndexSet(D=D, D_=D_, M=M, K=K)
+    constant = Constant(beta=beta, phi=phi, g=g, P=P)
+    return index_set, constant
---
+++ src/optimize/models/POLR/model.py
@@ -0,0 +1,171 @@
+from __future__ import annotations
+
+import itertools
+import time
+from dataclasses import dataclass
+from pathlib import Path
+from typing import Optional
+
+import pulp
+from gurobipy import GurobiError
+
+from src.optimize.result import Result
+from src.utils.paths import DATA_DIR
+
+
+@dataclass(frozen=True)
+class IndexSet:
+    D: dict[str, list[int]]
+    D_: dict[str, list[int]]
+    M: list[str]
+    K: list[str]
+
+
+@dataclass(frozen=True)
+class Constant:
+    beta: dict[tuple[str, str], float]
+    phi: dict[tuple[str, str, int], float]
+    g: dict[str, float]
+    P: dict[tuple[str, int], float]
+    big_M: int = 1000000
+
+
+class Variable:
+    def __init__(self, index_set: IndexSet, constant: Constant):
+        self.index_set = index_set
+        self.constant = constant
+        self.p: dict[tuple[str], pulp.LpVariable] = dict()
+        self.q: dict[tuple[str], pulp.LpVariable] = dict()
+        self.u: dict[tuple[str, int], pulp.LpVariable] = dict()
+        self.x: dict[tuple[str, int], pulp.LpVariable] = dict()
+
+        self._set_variables()
+
+    def _set_variables(self) -> None:
+        for m in self.index_set.M:
+            self.p[m] = pulp.LpVariable(f"p[{m}]", cat=pulp.LpContinuous)
+            self.q[m] = pulp.LpVariable(f"q[{m}]", cat=pulp.LpContinuous)
+            for k in self.index_set.K:
+                self.x[m, k] = pulp.LpVariable(f"x[{m}_{k}]", cat=pulp.LpBinary)
+                self.u[m, k] = pulp.LpVariable(f"u[{m}_{k}]", cat=pulp.LpContinuous)
+
+    def to_value(self):
+        for attr in self.__dict__.keys():
+            if attr not in ["index_set", "constant"]:
+                for k, v in self.__dict__[attr].items():
+                    self.__dict__[attr][k] = v.value()
+
+
+class ObjectiveFunctionMixin:
+    index_set: IndexSet
+    constant: Constant
+    variable: Variable
+    problem: pulp.LpProblem
+
+    def set_objective_function(self) -> None:
+        objective_function = 0
+        for m, k in itertools.product(self.index_set.M, self.index_set.K):
+            objective_function += self.constant.P[m, k] * self.variable.u[m, k]
+        self.problem += objective_function, "Objective Function"
+
+
+class ConstraintsMixin:
+    index_set: IndexSet
+    constant: Constant
+    variable: Variable
+    problem: pulp.LpProblem
+
+    def set_constraints(self) -> None:
+        self._set_q_constraints()
+        self._set_price_constraints()
+        self._set_p_constraints()
+        self._set_u_constraints()
+
+    def _set_q_constraints(self) -> None:
+        for m in self.index_set.M:
+            self.problem += self.variable.q[m] == pulp.lpSum(
+                self.constant.beta[m, "PRICE" + "_" + mp] * self.variable.p[mp]
+                for mp in self.index_set.M
+            ) - pulp.lpSum(
+                self.constant.beta[m, d] * self.constant.g[d]
+                for d in self.index_set.D[m] + self.index_set.D_[m]
+            )
+
+    def _set_price_constraints(self) -> None:
+        for m in self.index_set.M:
+            self.problem += pulp.lpSum(self.variable.x[m, k] for k in self.index_set.K) == 1
+
+    def _set_p_constraints(self) -> None:
+        for m in self.index_set.M:
+            self.problem += self.variable.p[m] == pulp.lpSum(
+                self.constant.P[m, k] * self.variable.x[m, k] for k in self.index_set.K
+            )
+
+    def _set_u_constraints(self) -> None:
+        for m, k in itertools.product(self.index_set.M, self.index_set.K):
+            self.problem += -self.constant.big_M * self.variable.x[m, k] <= self.variable.u[m, k]
+            self.problem += self.variable.u[m, k] <= self.constant.big_M * self.variable.x[m, k]
+            self.problem += (
+                self.variable.q[m] - self.constant.big_M * (1 - self.variable.x[m, k])
+                <= self.variable.u[m, k]
+            )
+            self.problem += self.variable.u[m, k] <= self.variable.q[m] + self.constant.big_M * (
+                1 - self.variable.x[m, k]
+            )
+
+
+class Model(ObjectiveFunctionMixin, ConstraintsMixin):
+    index_set: IndexSet
+    constant: Constant
+    variable: Variable
+    problem: pulp.LpProblem
+
+    def __init__(self, index_set: IndexSet, constant: Constant):
+        self.index_set = index_set
+        self.constant = constant
+        self.name = "POLR"
+        self.result = None
+        self.calculation_time = None
+        self.objective = None
+
+    def _set_model(self) -> None:
+        self.problem = pulp.LpProblem(name=self.name, sense=pulp.LpMaximize)
+        self.variable = Variable(self.index_set, self.constant)
+        self.set_objective_function()
+        self.set_constraints()
+
+    def write_lp(self, dir_path: Optional[Path] = DATA_DIR / "results" / "lpfile") -> None:
+        self.problem.writeLP(str(dir_path / f"{self.name}.lp"))
+
+    def solve(
+        self,
+        solver: Optional[str] = None,
+        TimeLimit: int = 100,
+        NoRelHeurTime: float = 0,
+        MIPFocus: int = 0,
+        write_lp: bool = False,
+    ) -> None:
+        # モデルを構築
+        self._set_model()
+        # solverの設定
+        if solver in ["gurobi", "GUROBI", "Gurobi", "grb", "GRB", "Grb"]:
+            try:
+                solver = pulp.GUROBI(
+                    timeLimit=TimeLimit, NoRelHeurTime=NoRelHeurTime, MIPFocus=MIPFocus
+                )
+            except GurobiError:
+                raise Exception("Set the solver to Cbc because Gurobi is not installed.")
+        else:
+            solver = pulp.PULP_CBC_CMD(timeLimit=TimeLimit)
+        # 求解
+        start = time.time()
+        self.problem.solve(solver=solver)
+        elapsed_time = time.time() - start
+        # 結果の格納
+        self.objective = self.problem.objective.value()
+        self.variable.to_value()
+        self.result = Result(
+            calculation_time=elapsed_time, objective=self.objective, opt_prices=self.variable.x
+        )
+        if write_lp:
+            self.write_lp()
