0c9baf231f2abd616feac7586391869fac07d54a
sns-ikeda <ikeda.shunnosuke@gmail.com>
2022-08-10 23:09:09 +0900

    preprocessorの拡充

--- src/data_preprocess/preprocessor.py
+++ src/data_preprocess/preprocessor.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import TypeVar
+from typing import Optional, TypeVar
 
 import numpy as np
 import pandas as pd
@@ -37,18 +37,6 @@ class DataPreprocessor:
         self.preprocessed = True
         return self.processed_df
 
-    @staticmethod
-    def get_item2prices(
-        df: pd.DataFrame, num_of_prices: int, items: list[str], prefix: str = "PRICE"
-    ) -> dict[str, list[float]]:
-        item2prices = dict()
-        for item in items:
-            price_col = prefix + "_" + item
-            price_max = df[price_col].max()
-            price_min = df[price_col].min()
-            item2prices[item] = list(np.linspace(price_min, price_max, num_of_prices))
-        return item2prices
-
     def get_target_cols(self, prefix: str = "UNITS") -> list[str]:
         if self.preprocessed:
             target_cols = [col for col in self.processed_df.columns if prefix in col]
@@ -63,31 +51,61 @@ class DataPreprocessor:
             raise Exception("Run preprocess before executing this method")
         return feature_cols
 
-    @staticmethod
-    def get_label2item(target_cols: list[str]) -> dict[str, str]:
-        label2item = dict()
-        for col in target_cols:
-            item = col.split("_")[-1]
-            label2item[col] = item
-        return label2item
+
+def get_label2item(target_cols: list[str]) -> dict[str, str]:
+    label2item = dict()
+    for col in target_cols:
+        item = col.split("_")[-1]
+        label2item[col] = item
+    return label2item
 
 
-def select_scaler(scaling_type: str = "standard"):
+def get_item2avg_prices(
+    df: pd.DataFrame, items: list[str], prefix: str = "PRICE"
+) -> dict[str, float]:
+    avg_prices = dict()
+    for item in items:
+        price_col = prefix + "_" + item
+        avg_prices[item] = df[price_col].mean()
+    return avg_prices
+
+
+def get_item2prices(
+    df: pd.DataFrame, num_of_prices: int, items: list[str], prefix: str = "PRICE"
+) -> dict[str, list[float]]:
+    item2prices = dict()
+    for item in items:
+        price_col = prefix + "_" + item
+        price_max = df[price_col].max()
+        price_min = df[price_col].min()
+        item2prices[item] = list(np.linspace(price_min, price_max, num_of_prices))
+    return item2prices
+
+
+def select_scaler(scaling_type: Optional[str] = None):
     from sklearn import preprocessing
 
-    if scaling_type == "standard":
+    if scaling_type is None:
+        return None
+    elif scaling_type == "standard":
         return preprocessing.StandardScaler()
     elif scaling_type == "minmax":
         return preprocessing.MinMaxScaler()
 
 
 def inverse_transform(
-    scaler: ScalerClass, item2prices: dict[str, list[float]], X_train: pd.DataFrame
+    scaler: ScalerClass, item2prices: dict[str, list[float]], X_df: pd.DataFrame
 ) -> dict[str, list[float]]:
+    _X_df = X_df.copy()
+    for item, price in item2prices.items():
+        price_col = "PRICE" + "_" + item
+        _X_df[price_col] = price
+
     item2inv_prices = dict()
-    return item2inv_prices
-
-
-def get_avg_prices(X_train: pd.DataFrame, items: list[str]) -> dict[str, float]:
-    avg_prices = dict()
-    return avg_prices
+    if scaler is not None:
+        _X_inv = pd.DataFrame(scaler.inverse_transform(_X_df), columns=_X_df.columns.tolist())
+        for item in item2prices.keys():
+            price_col = "PRICE" + "_" + item
+            item2inv_prices[item] = float(_X_inv[price_col].iloc[0])
+    else:
+        return item2prices
