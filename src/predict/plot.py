8ef980cef46653e032f38ccb461aeff7c67d4779
sns-ikeda <ikeda.shunnosuke@gmail.com>
2022-08-23 22:49:57 +0900

    plotを修正

--- src/predict/plot.py
+++ src/predict/plot.py
@@ -6,7 +6,6 @@ import pandas as pd
 
 
 def plot(
-    X: pd.DataFrame,
     y: pd.DataFrame,
     y_pred: pd.DataFrame,
     predictor_name: str,
@@ -15,9 +14,7 @@ def plot(
     suffix: Optional[str] = None,
 ):
     # 結果をプロット
-    x_col = "PRICE" + "_" + target_item
-    plt.scatter(X[x_col], y, color="blue", label="actual")  # 説明変数と目的変数のデータ点の散布図をプロット
-    plt.scatter(X[x_col], y_pred, color="red", label="predicted")  # 回帰直線をプロット
+    plt.scatter(y_pred, y, color="blue")  # 横軸に予測値，縦軸に実測値
     if suffix is None:
         plt.title(f"{predictor_name}: {target_item}")  # 図のタイトル
         save_name = f"{predictor_name}_{target_item}.png"
@@ -25,10 +22,8 @@ def plot(
         plt.title(f"{predictor_name}: {target_item} [{suffix}]")  # 図のタイトル
         save_name = f"{predictor_name}_{target_item}_{suffix}.png"
 
-    plt.xlabel("Price[$]")  # x軸のラベル
-    plt.ylabel("# of Units")  # y軸のラベル
+    plt.xlabel("actual units")  # x軸のラベル
+    plt.ylabel("predicted units")  # y軸のラベル
     plt.grid()  # グリッド線を表示
-    plt.legend()
-    # plt.show()
     plt.savefig(Path(dir_path) / save_name, format="png", dpi=300)
     plt.clf()
