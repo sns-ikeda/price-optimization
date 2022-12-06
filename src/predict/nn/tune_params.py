5482e8c6872fd6d5b7ece9a1cea72f85ed7398a2
sns-ikeda <ikeda.shunnosuke@gmail.com>
2022-12-06 13:37:37 +0900

    nn削除

--- src/predict/nn/tune_params.py
+++
@@ -1,8 +0,0 @@
-from __future__ import annotations
-
-import pandas as pd
-
-
-def tune_params(X: pd.DataFrame, y: pd.DataFrame, **kwargs) -> dict[str, float]:
-    best_params = dict()
-    return best_params
