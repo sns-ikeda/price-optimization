cbd84667240aa220bae54c1694628bb9df8478a6
sns-ikeda <ikeda.shunnosuke@gmail.com>
2022-12-06 13:36:01 +0900

    ridge -> lasso

--- src/predict/linear_regression/train.py
+++ src/predict/linear_regression/train.py
@@ -6,7 +6,7 @@ from typing import Optional
 
 import pandas as pd
 from logzero import logger
-from sklearn.linear_model import Ridge
+from sklearn.linear_model import Lasso
 
 from src.predict.predictor import Predictor
 
@@ -24,8 +24,8 @@ def train(
     else:
         params_ = copy.deepcopy(params)
     # 学習
-    logger.info("fitting by ridge...")
-    model = Ridge(**params_)
+    logger.info("fitting by lasso...")
+    model = Lasso(**params_)
     model.fit(X, y)
     predictor = Predictor(model=model, feature_cols=feature_cols, target_col=target_col)
     logger.info(f"coefficients: {model.coef_[0]}")
