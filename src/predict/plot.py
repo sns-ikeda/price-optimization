6a8d954f4de518a2750013e5fa89ed2701753ed0
sns-ikeda <ikeda.shunnosuke@gmail.com>
2022-12-13 16:32:50 +0900

    plotの修正

--- src/predict/plot.py
+++ src/predict/plot.py
@@ -15,7 +15,7 @@ def plot(
     suffix: Optional[str] = None,
 ):
     # 結果をプロット
-    plt.scatter(y, y_pred, color="blue")  # 横軸に予測値，縦軸に実測値
+    plt.scatter(y_pred, y, color="blue")  # 横軸に予測値，縦軸に実測値
     if suffix is None:
         plt.title(f"{predictor_name}: {target_item}")  # 図のタイトル
         save_name = f"{predictor_name}_{target_item}.png"
@@ -23,8 +23,8 @@ def plot(
         plt.title(f"{predictor_name}: {target_item} [{suffix}]")  # 図のタイトル
         save_name = f"{predictor_name}_{target_item}_{suffix}.png"
 
-    plt.xlabel("actual units")  # x軸のラベル
-    plt.ylabel("predicted units")  # y軸のラベル
+    plt.xlabel("predicted units")  # x軸のラベル
+    plt.ylabel("actual units")  # y軸のラベル
     plt.xlim(0, max(max(np.array(y))[0], max(y_pred)) * 1.1)
     plt.ylim(0, max(max(np.array(y))[0], max(y_pred)) * 1.1)
     plt.grid()  # グリッド線を表示
