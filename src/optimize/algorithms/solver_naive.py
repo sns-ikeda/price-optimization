a48d820afdcb67f4a00410c120d25e7ff8ccca98
sns-ikeda <ikeda.shunnosuke@gmail.com>
2023-04-30 00:16:40 +0900

    solver_naive.pyのリファクタ

--- src/optimize/algorithms/solver_naive.py
+++ src/optimize/algorithms/solver_naive.py
@@ -1,10 +1,12 @@
-from typing import Optional
+from typing import Optional, TypeVar
 
 from src.optimize.algorithms.base_algorithm import BaseAlgorithm
 
+Model = TypeVar("Model")
+
 
 class SolverNaive(BaseAlgorithm):
-    def __init__(self, model, solver: str = "Cbc", TimeLimit: Optional[int] = None):
+    def __init__(self, model: Model, solver: str = "Cbc", TimeLimit: Optional[int] = None):
         super().__init__(model)
         self.solver = solver
         self.TimeLimit = TimeLimit
